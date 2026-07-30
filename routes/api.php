6cd84676ae335c42e3ee462682bc4c35942fcc1f
Ayoub-aalaou97 <aalaou.ayoub97@gmail.com>
2026-07-30 14:02:05 +0200

    Initial laravel  setup

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/ping', function () {
+    return response()->json(['ok' => true, 'app' => 'habitLoop']);
+});
