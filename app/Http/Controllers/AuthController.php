e6f90e02b455a78de7ec25c8489edf4aad48a472
Ayoub-aalaou97 <aalaou.ayoub97@gmail.com>
2026-07-30 20:51:55 +0200

    HL-6 Register / login with email

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+
+        // validate
+        $validated = $request->validate([
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
+        // create user
+        $user = User::create([
+            'first_name' => $validated['first_name'],
+            'last_name' => $validated['last_name'],
+            'email' => $validated['email'],
+            'password' => $validated['password'],
+        ]);
+
+        // create sanctum tocken
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        // Return response
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $credentials['email'])->first();
+
+        if (! $user || ! Hash::check($credentials['password'], $user->password)) {
+            return response()->json([
+                'message' => 'The provided credentials are incorrect.',
+            ], 422);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+
+    }
+
+    public function logout(Request $request)
+    {
+        // Delete the current token
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::get('/ping', function () {
-    return response()->json(['ok' => true, 'app' => 'habitLoop']);
-});
+//Registration (api test)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
