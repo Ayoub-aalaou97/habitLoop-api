1fe276f88886bac27170f7114127a413757a3af6
Ayoub-aalaou97 <aalaou.ayoub97@gmail.com>
2026-08-10 16:22:00 +0200

    Revert "HL-7 forgot password with email verification code"
    This reverts commit 86d01ee9918c98b31718383f59b2192a108110d2.

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PasswordResetController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,11 +13,5 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::post('/forgot-password', [PasswordResetController::class, 'forgotPassword'])
-    ->middleware('throttle:5,1');
-Route::post('/verify-reset-code', [PasswordResetController::class, 'verifyCode'])
-    ->middleware('throttle:10,1');
-Route::post('/reset-password', [PasswordResetController::class, 'resetPassword'])
-    ->middleware('throttle:10,1');
 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
